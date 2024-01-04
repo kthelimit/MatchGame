973ea239744718da088f16307076b42db74d8f28
WD <WD@WINDOWS-Q8ENQ74>
2024-01-04 18:36:44 +0900

    초기 커밋.

---
+++ MatchGame/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MatchGame
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            SetUpGame();
+        }
+
+        private void SetUpGame()
+        {
+            List<string> animalEmoji = new List<string>()
+            {
+                "🥞", "🥞",
+                "🥨", "🥨",
+                "🥗", "🥗",
+                "🍱", "🍱",
+                "🍤", "🍤",
+                "🍙", "🍙",
+                "🍜", "🍜",
+                "🌭", "🌭",
+            };
+            Random random = new Random();
+            foreach(TextBlock textBlock in mainGrid.Children.OfType<TextBlock>())
+            {
+                if (textBlock.Name !="timeTextBlock")
+                {
+                    textBlock.Visibility = Visibility.Visible;
+                    int index = random.Next(animalEmoji.Count);
+                    string nextemoji = animalEmoji[index];
+                    textBlock.Text = nextemoji;
+                    animalEmoji.RemoveAt(index);
+                }
+            }
+        }
+    }
+}
